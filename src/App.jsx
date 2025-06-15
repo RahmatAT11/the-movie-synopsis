c614a4bce15ad27ed5c1634cccec72310939f331
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2025-06-15 17:16:13 +0700

    feat: Finish styling header part

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import Search from './components/search';
+import React from "react";
+import Search from "./components/search";
+import { useState } from "react";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <main>
-      <div className='pattern' />
+      <div className="pattern" />
       <div className="wrapper">
         <header>
           <img src="./hero-img.png" alt="Hero Banner" />
-          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
         </header>
 
-        <Search />
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className="text-white">{searchTerm}</h1>
       </div>
     </main>
   );
-}
+};
 
 export default App;
